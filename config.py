08441313d0e008aa6acc279e30baa97c9a6847ed
ummah272 <sitinurrohmatulummah3@gmail.com>
2026-06-17 16:28:45 +0700

    update config.py

--- config.py
+++ config.py
@@ -5,4 +5,8 @@ class Config:
     # Gantilah username dan password dengan yang sesuai dari Laragon
     SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/vida'  # username=root, password=kosong
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        'pool_pre_ping': True,
+        'pool_recycle' : 280
+    }
     SECRET_KEY = os.urandom(24)
